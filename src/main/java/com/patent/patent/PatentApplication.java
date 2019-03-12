a672e1814c520a9fe9e0436b13765bbb86ab1c68
psmkab1 <psmkab1@coupang.com>
2019-03-13 02:07:09 +0900

    modify .gitignore & add swagger example & update README.md

--- src/main/java/com/patent/patent/PatentApplication.java
+++ src/main/java/com/patent/patent/PatentApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PatentApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(PatentApplication.class, args);
     }
-
 }
--- src/main/java/com/patent/patent/controller/TestController.java
+++ src/main/java/com/patent/patent/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.patent.patent.controller;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configurable
+@Api(value = "test controller")
 @RestController
+@RequestMapping(value = "/api")
 public class TestController {
 
+    @ApiOperation(value = "test controller")
     @GetMapping("/test")
     public String hello(){
         return "hello";
